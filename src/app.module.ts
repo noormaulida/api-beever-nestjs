9127aea3994db15707e9847a4541261fb342a830
Noor Maulida <noormaulida.dev@gmail.com>
2024-10-07 16:33:41 +0700

    #2 Initialize Sequalize and DB Credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+import pg from 'pg';
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      dialectModule: pg,
+      host: 'ls-84a050d3c8054bff6a276d7fbacb38f6dffda0cb.c3psaafrxcpx.ap-southeast-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'dbmasteruser',
+      password: 'testobnb1234567890',
+      database: 'noormaulida',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
